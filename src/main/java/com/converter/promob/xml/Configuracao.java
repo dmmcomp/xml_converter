625fa91fdbc2d946ea8a02dcbaa26f65015b0224
Diego Melo <cin_dmelo@uolinc.com>
2019-04-23 12:24:33 -0300

    alteracao

--- src/main/java/com/converter/promob/xml/Configuracao.java
+++ src/main/java/com/converter/promob/xml/Configuracao.java
@@ -1,11 +1,11 @@
-package com.converter.promob.xml.config;
+package com.converter.promob.xml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan("com.converter.promob.xml")
+
 @SpringBootApplication
 //@Controller
 public class Configuracao {
